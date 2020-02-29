11569ab562df6153ffaf63f94aeef55f649215c6
anjaliso <gitapatil17@gmail.com>
2020-02-29 17:17:28 +0530

    Initial git commands

---
+++ gitcommands.js
@@ -0,0 +1,15 @@
+// Git Commands
+"git add .",
+  "git commit -m",
+  "git push origin branch_name",
+  "git pull",
+  "git clone repo_url",
+  "git remote add origin url.git";
+("git remote removr origin");
+("git checkout -b new_branch_name"); // To create branch
+
+// Information commnds
+("git branch");
+("git status");
+("git log");
+("git log --decorate --oneline --graph");
